8932f76fda9efe39085bbb78fbaba20c603dd8d8
Winicius Sturm <winiciussturm@gmail.com>
2018-09-08 21:57:08 -0300

    Tratando erros 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -31,7 +31,17 @@ export class ProfilePage {
           this.cliente = response;
           this.getImageIfExists();
         },
-        error => {})
+        error => 
+        {
+          if (error.status == 403)
+          {
+            this.navCtrl.setRoot("HomePage");
+          }
+        })
+    }
+    else
+    {
+      this.navCtrl.setRoot("HomePage");
     }
   }
 
